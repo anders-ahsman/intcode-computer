a24d9ea0e5e2ab61e391c8726c5c04a58c9ff820
Anders Åhsman <anders.ahsman@gmail.com>
2020-03-15 09:25:24 +0100

    Fixed spelling in test name.

--- test_handler.py
+++ test_handler.py
@@ -74,7 +74,7 @@ def test_valid_input__responds_with_program_output(mock_boto3):
     assert msg == '42'
 
 @patch('handler.boto3')
-def test_valid_input__repsonse_ends_with_program_completed_message(mock_boto3):
+def test_valid_input__response_ends_with_program_completed_message(mock_boto3):
     mock_apigw = Mock()
     mock_boto3.client.return_value = mock_apigw
     event['body'] = json.dumps({'program': [3,0,4,0,99], 'input': 42})
